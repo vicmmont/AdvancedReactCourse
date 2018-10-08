329eb61d8606b48e75178a2b1c8be59851dfffc3
vicmmont <vmm420@gmail.com>
2018-10-07 20:26:36 -0400

    Create Prisma Server, GraphQL Server
    + User data model created
    + Prisma configured
    + db configuration created
    + GraphQL yoga server configured
      + cors settings set up
    + created dummy mutation, query

--- public/backend/src/index.js
+++ public/backend/src/index.js
@@ -1,1 +1,19 @@
-// lets go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middleware to handle cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start(
+  {
+    cors: { credentials: true, origin: process.env.FRONT_END_URL }
+  },
+  details => {
+    console.log(
+      `Server is now running on port http://localhost:${details.port}`
+    );
+  }
+);
