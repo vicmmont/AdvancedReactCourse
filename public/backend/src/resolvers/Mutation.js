35d6a40ef983ddc7c743bd463cc2964a1463bb79
vicmmont <vmm420@gmail.com>
2018-10-30 23:39:54 -0400

    Uploading images, updating items
    + Created wloudinary account and hooked it up to createItem page to
    store images
    + Created page to update item
    + Created query and mutation resolvers to fetch an item and to update it
    + UpdateItem component created

--- public/backend/src/resolvers/Mutation.js
+++ public/backend/src/resolvers/Mutation.js
@@ -11,6 +11,21 @@ const Mutations = {
     );
 
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // Take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
   }
 };
 
