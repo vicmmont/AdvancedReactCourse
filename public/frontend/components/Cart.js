7fe69362cb2c0e557ed409f0b58ea4b756cc5891
vicmmont <vmm420@gmail.com>
2019-02-17 00:26:01 -0500

    Making Cart Component Toggleable
    + Cart component exports Mutation and Query for local state
    + Local state for cart status is implemented in apollo client
    + Cart Button added to navigation

---
+++ public/frontend/components/Cart.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+export const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
+const Cart = props => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton onClick={toggleCart} title="close">
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You have __ items in your cart</p>
+            </header>
+            <footer>
+              <p>$10.10</p>
+              <SickButton>Check Out</SickButton>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
+);
+
+export default Cart;
--- public/frontend/components/Nav.js
+++ public/frontend/components/Nav.js
@@ -3,6 +3,8 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 import { Fragment } from 'react';
 import SignOut from './SignOut';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from '../components/Cart';
 
 const Nav = () => (
   <User>
@@ -15,6 +17,9 @@ const Nav = () => (
             <Link href="/orders">Orders</Link>
             <Link href="/me">Account</Link>
             <SignOut />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </Fragment>
         )}
 
